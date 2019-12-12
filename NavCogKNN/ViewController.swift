9c8c27c12badabc3149626add63a98fca8294531
Vivek Roy <thezealousfool@gmail.com>
2019-12-12 17:19:33 -0500

    Initial Commit

---
+++ NavCogKNN/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavCogKNN
+//
+//  Created by Vivek Roy on 12/12/19.
+//  Copyright © 2019 Vivek Roy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
